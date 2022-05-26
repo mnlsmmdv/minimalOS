d30aa43370f089b51d12f84f6c366aea45be8eb8
a2-affaan <ahmed2.affaan@live.uwe.ac.uk>
2022-05-26 17:41:01 +0500

    File description set and comments added for understanding program

--- kernel.c
+++ kernel.c
@@ -1,4 +1,18 @@
 /*
+ * Name: Ahmed Affaan
+ * Title: kernel.c
+ * Folder: minimalOS
+ * Date: 26/05/2022
+ * Country: Republic of Maldives
+ * Description: Minimal OS, to show how to boot an x86 machine. UFCFWK-15-2
+ * Student Number(UWE): 19045165
+ * Student ID:(VC): 1802035
+ * Note: Uncomment codes to execute and comment them when not in use.
+ */
+
+// Program start.
+
+/*
  * Desc: Minimal OS, to show how to boot an x86 machine. UFCFWK-15-2
  */
 #if !defined(__cplusplus)
@@ -36,17 +50,20 @@ enum vga_color {
 	COLOR_LIGHT_BROWN = 14,
 	COLOR_WHITE = 15,
 };
- 
+
+// This function will choose colours for the kernel. 
 uint8_t make_color(enum vga_color fg, enum vga_color bg) {
   return fg | bg << 4;
 }
- 
+
+// This function will creates the characters colours needed for the kernel.
 uint16_t make_vgaentry(char c, uint8_t color) {
   uint16_t c16 = c;
   uint16_t color16 = color;
   return c16 | color16 << 8;
 }
- 
+
+// This function will validate strings. 
 size_t strlen(const char* str) {
   size_t ret = 0;
   while ( str[ret] != 0 )
@@ -54,14 +71,22 @@ size_t strlen(const char* str) {
   return ret;
 }
 
+// Variable Declarations.
+// Width of the terminal screen.
 static const size_t VGA_WIDTH = 80;
+// Height of the terminal screen.
 static const size_t VGA_HEIGHT = 25;
 
+// Refers to termninal row.
 size_t terminal_row;
+// Refers to terminal column.
 size_t terminal_column;
+// Refers to terminal's colour.
 uint8_t terminal_color;
+// Refers to terminal's buffer.
 uint16_t* terminal_buffer;
  
+// This function will refresh the programs terminal screen.
 void terminal_initialize() {
   terminal_row = 0;
   terminal_column = 0;
@@ -75,15 +100,18 @@ void terminal_initialize() {
   }
 }
  
+// This function will set the terminal's colours.
 void terminal_setcolor(uint8_t color) {
   terminal_color = color;
 }
  
+// This function will display various characters on the terminal screen.
 void terminal_putentryat(char c, uint8_t color, size_t x, size_t y) {
   const size_t index = y * VGA_WIDTH + x;
   terminal_buffer[index] = make_vgaentry(c, color);
 }
 
+// This function will display the current position of the cursor on the terminal screen.
 void terminal_putchar(char c) {
   terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
   if (++terminal_column == VGA_WIDTH) {
@@ -94,12 +122,14 @@ void terminal_putchar(char c) {
   }
 }
  
+// This function will help print characters on the terminal screen.
 void terminal_writestring(const char* data) {
   size_t datalen = strlen(data);
   for (size_t i = 0; i < datalen; i++)
     terminal_putchar(data[i]);
 }
 
+// Programs main function.
 #if defined(__cplusplus)
 extern "C" /* Use C linkage for kernel_main. */
 #endif
@@ -113,3 +143,6 @@ void kernel_main() {
    */
   terminal_writestring("Hello, kernel World!\n");
 }
+
+// Program end.
+
